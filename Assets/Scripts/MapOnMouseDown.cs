9fb5084165886d4bc18c397bc865e48a2a4766d2
klambo94@gmail.com <klambo94@gmail.com>
2019-04-27 22:08:08 -0400

    Inital Commit for LifeCurrency Unity Project
    Includes Work done for Hospital/Level work

---
+++ Assets/Scripts/MapOnMouseDown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MapOnMouseDown : MonoBehaviour
+{
+    
+    void OnMouseUpAsButton()
+    {
+        
+        Debug.Log("This is a mouse click");
+        // load a new scene
+        SceneManager.LoadScene("HospitalScene");
+    }
+}
